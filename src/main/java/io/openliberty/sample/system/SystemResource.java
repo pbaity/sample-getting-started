b55f4451f4e53b3d0a947a3d84ae76215e8847fd
Jimmy Wu <jimmywu0605@gmail.com>
2023-05-02 14:37:06 -0400

    test

--- src/main/java/io/openliberty/sample/system/SystemResource.java
+++ src/main/java/io/openliberty/sample/system/SystemResource.java
@@ -32,7 +32,7 @@ public class SystemResource {
 	SystemConfig systemConfig;
 
 	@GET
-	@RolesAllowed({ "user" })
+	// @RolesAllowed({ "user" })
 	@Produces(MediaType.APPLICATION_JSON)
 	@Timed(name = "getPropertiesTime", description = "Time needed to get the properties of a system")
 	@Counted(absolute = true, description = "Number of times the properties of a systems is requested")
